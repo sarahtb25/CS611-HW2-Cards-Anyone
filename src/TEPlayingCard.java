86a1a72b93157501304a823eb52efe21627617a3
Sarah Taaher Bonna <sbonna@bu.edu>
2022-10-08 17:01:47 -0400

    Added TEPlayingCard to specify card values for the playing cards specific to TE

---
+++ src/TEPlayingCard.java
@@ -0,0 +1,20 @@
+public class TEPlayingCard extends PlayingCard {
+
+    TEPlayingCard(int id, char suit, boolean isFaceDown) {
+        super(id, suit, isFaceDown);
+    }
+
+    @Override
+    public void setPlayingCardValue(int id) {
+        int value;
+        if (id >= 2 && id <= 10) {
+            value = id;
+        } else if (id == 1) { // A
+            value = 11;
+        } else { // J, Q, K
+            value = 10;
+        }
+
+        setValue(value);
+    }
+}
