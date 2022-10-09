d3f4101c40bb270590b6201b8fdb5cc7f8f7dd5a
HK3121 <114822500+harsh242bu@users.noreply.github.com>
2022-10-09 11:25:49 -0400

    Added calculateWinner method to Game interface

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,13 @@
+import java.util.List;
+
 // Here we play the game
 public interface Game {
 
     public void initializePlayers();
 
     public void playGame();
+
+    public List<TECardPlayer> calculateWinner();
 
 //    public void playRound();
 
